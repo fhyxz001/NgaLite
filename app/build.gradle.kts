66570b1b1d394c128bacae4fb9e1e4012f142d17
hexb <919760638@qq.com>
2026-07-02 01:02:25 +0800

    发布v1.70

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ngalite.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.69"
+        versionCode = 31
+        versionName = "1.70"
     }
 
     signingConfigs {
